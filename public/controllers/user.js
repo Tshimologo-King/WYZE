40ec1c772d44fe8ca351bc15163e4d09827c4506
King <tshimologokingmabena@gmail.com>
2022-09-09 11:41:36 +0200

    updated subscribers route

--- public/controllers/user.js
+++ public/controllers/user.js
@@ -5,35 +5,38 @@ require("dotenv").config();
 
 //ADD SUSBSCRIPTION USER
 async function addSubscription(req, res) {
-    const { idSubscriptions, status, userName, userEmail } = req.body;
-    try{
-        connection.query(`INSERT INTO subscriptions(status, userName, userEmail) VALUES ("${status}", "${userName}", "${userEmail}")`,
-        (err, result) => {
-            if (err) throw err;
-            res.send(result);
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(404).send(error);
-    }
+  const { idSubscriptions, status, userName, userEmail } = req.body;
+  try {
+    connection.query(
+      `INSERT INTO subscribers(status, userName, userEmail) VALUES ("${status}", "${userName}", "${userEmail}")`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(404).send(error);
+  }
 }
 
 //DELETE SUBSCRIPTION
 async function cancelSubscription(req, res) {
-    try {
-      connection.query(
-        `DELETE FROM subscribers WHERE idsubscriber=${req.params.id}`,
-        (err, result) => {
-          if (err) throw err;
-          res.send(result);
-        }
-      );
-    } catch (error) {
-      console.log(error);
-      res.status(400).send(error);
-    }
+  try {
+    connection.query(
+      `DELETE FROM subscribers WHERE idsubscriber=${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
 }
 
 module.exports = {
-    addSubscription, cancelSubscription
+  addSubscription,
+  cancelSubscription,
 };
